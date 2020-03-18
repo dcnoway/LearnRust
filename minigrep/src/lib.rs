1b1a5df6f2a00773c43824a8f31b25727c8747d9
Will Wu <willswu@outlook.com>
2020-03-18 19:40:04 +0800

    add unit test

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -6,7 +6,7 @@ pub fn run(cfg: &Config) -> Result<(), Box<dyn Error>> {
     // let mut hit_lines: Vec<&str> = vec![];
     let content = fs::read_to_string(&cfg.filename)?;
     let hit_lines = search(&cfg.query, &content);
-    for (line_number,line) in hit_lines.iter().enumerate() {
+    for (line_number,line) in hit_lines.iter() {
         println!("{:>6}:{}",line_number,line);
     }
     return Ok(());
@@ -30,11 +30,11 @@ impl Config {
     }
 }
 
-pub fn search<'a>(query: &str,contents: &'a str) -> Vec<&'a str> {
-    let mut result:Vec<&str>=vec![];
-    for line in contents.lines() {
+pub fn search<'a>(query: &str,contents: &'a str) -> Vec<(usize,&'a str)> {
+    let mut result:Vec<(usize,&'a str)> =vec![];
+    for (i,line) in contents.lines().enumerate() {
         if line.contains(query){
-            result.push(line);
+            result.push((i+1,line));
         }
     }
     result
@@ -47,6 +47,7 @@ mod tests {
     fn one_result() {
         let query = "duct";
         let content = "Rust:\nsafe, fast, productive.\nPick three.";
-        assert_eq!(vec!["safe, fast, productive."], search(query, content));
+        assert_eq!("safe, fast, productive.", search(query, content)[0].1);
+        assert_eq!(1,search(query, content).len());
     }
 }
