c0c4106a54ae23013c265228c9e8c0c3760ca56b
Will Wu <willswu@outlook.com>
2020-03-19 01:16:17 +0800

    use iter trait

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -6,8 +6,8 @@ pub fn run(cfg: &Config) -> Result<(), Box<dyn Error>> {
     // let mut hit_lines: Vec<&str> = vec![];
     let content = fs::read_to_string(&cfg.filename)?;
     let hit_lines = search(&cfg.query, &content);
-    for (line_number,line) in hit_lines.iter() {
-        println!("{:>6}:{}",line_number,line);
+    for (line_number, line) in hit_lines.iter() {
+        println!("{:>6}:{}", line_number, line);
     }
     return Ok(());
 }
@@ -19,25 +19,39 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        args.next(); //skip the first arg that is the fullpath of the binary file
+        let query: String;
+        let filename: String;
+        if let Some(s) = args.next() {
+            query = s;
+        } else {
+            return Err("Missing query parten!");
         }
-        Ok(Config {
-            query: String::from(&args[1]),
-            filename: String::from(&args[2]),
-        })
+        if let Some(s) = args.next() {
+            filename = s;
+        } else {
+            return Err("Missing file name!");
+        }
+        // if args.len() < 3 {
+        //     return Err("Not enough arguments");
+        // }
+        Ok(Config { query, filename })
     }
 }
 
-pub fn search<'a>(query: &str,contents: &'a str) -> Vec<(usize,&'a str)> {
-    let mut result:Vec<(usize,&'a str)> =vec![];
-    for (i,line) in contents.lines().enumerate() {
-        if line.contains(query){
-            result.push((i+1,line));
-        }
-    }
-    result
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<(usize, &'a str)> {
+    // let mut result: Vec<(usize, &'a str)> = vec![];
+    // for (i, line) in contents.lines().enumerate() {
+    //     if line.contains(query) {
+    //         result.push((i + 1, line));
+    //     }
+    // }
+    // result
+    contents.lines()
+        .enumerate()
+        .filter(|(_,line)| line.contains(query))
+        .collect()
 }
 
 #[cfg(test)]
@@ -48,6 +62,6 @@ mod tests {
         let query = "duct";
         let content = "Rust:\nsafe, fast, productive.\nPick three.";
         assert_eq!("safe, fast, productive.", search(query, content)[0].1);
-        assert_eq!(1,search(query, content).len());
+        assert_eq!(1, search(query, content).len());
     }
 }
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -2,11 +2,8 @@ use minigrep::Config;
 use std::env;
 use std::process;
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    println!("Args:\n{:?}", args);
-    // let cfg = Config::new(query,filename);
-    let cfg = Config::new(&args).unwrap_or_else(|err| {
-        println!("Arguments error: {:?}", err);
+    let cfg = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Arguments error: {:?}", err);
         process::exit(1);
     });
     println!("Query {} from file: {}", cfg.query, cfg.filename);
