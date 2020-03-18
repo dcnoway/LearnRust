888af82af396870372076bdbd53edf0ba9a36332
Will Wu <willswu@outlook.com>
2020-03-19 01:15:14 +0800

    try some iter features

--- TryIter/src/lib.rs
+++ TryIter/src/lib.rs
@@ -1,17 +1,63 @@
 #[test]
-fn iter_demo(){
-    let v1 = vec![1,2,3];
+fn iter_demo() {
+    let v1 = vec![1, 2, 3];
     let mut v1_iter = v1.iter();
-    assert_eq!(v1_iter.next().unwrap(),&1);
-    assert_eq!(v1_iter.next().unwrap(),&2);
-    assert_eq!(v1_iter.next().unwrap(),&3);
-    assert_eq!(v1_iter.next(),None);
+    assert_eq!(v1_iter.next().unwrap(), &1);
+    assert_eq!(v1_iter.next().unwrap(), &2);
+    assert_eq!(v1_iter.next().unwrap(), &3);
+    assert_eq!(v1_iter.next(), None);
 }
 
 #[test]
-fn iter_sum(){
-    let v1 = vec![1,2,3];
+fn iter_sum() {
+    let v1 = vec![1, 2, 3];
     let v1_iter = v1.iter();
-    let total:i32 = v1_iter.sum();
-    assert_eq!(total,6);
+    let total: i32 = v1_iter.sum();
+    assert_eq!(total, 6);
+}
+
+#[cfg(test)]
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u32,
+    style: String,
+}
+#[cfg(test)]
+fn shoes_in_my_size(shoes: &Vec<Shoe>, shoe_size: u32) -> Vec<&Shoe> {
+    shoes.iter().filter(|shoe| shoe.size == shoe_size).collect()
+}
+
+#[test]
+fn filters_by_size() {
+    let in_my_size: Vec<&Shoe>;
+    let shoes = vec![
+        Shoe {
+            size: 10,
+            style: String::from("sneaker"),
+        },
+        Shoe {
+            size: 13,
+            style: String::from("sandal"),
+        },
+        Shoe {
+            size: 10,
+            style: String::from("boot"),
+        },
+    ];
+    println!("{:?}", shoes);
+
+    in_my_size = shoes_in_my_size(&shoes, 10);
+    assert_eq!(
+        in_my_size,
+        vec![
+            &Shoe {
+                size: 10,
+                style: String::from("sneaker")
+            },
+            &Shoe {
+                size: 10,
+                style: String::from("boot")
+            },
+        ]
+    );
 }
--- TryIter/src/main.rs
+++ TryIter/src/main.rs
@@ -6,4 +6,37 @@ fn main() {
     for item in v1_iter {
         println!("{:?}", item);
     }
+
+    let v1:Vec<i32> = vec![1,2,3];
+    let v2:Vec<i32> = v1.iter().map(|x| x+1).collect();
+    println!("{:?}",v2);
+
+    let ctr = Counter::new().filter(|x| x%3 ==0);
+    for i in ctr{
+        println!("{}",i);
+    }
 }
+
+struct Counter{
+    count:u32,
+}
+
+impl Counter{
+    pub fn new()->Counter{
+        Counter{count:0}
+    }
+}
+
+impl Iterator for Counter{
+    type Item = u32;
+    fn next(&mut self) ->Option<Self::Item>{
+        self.count+=1;
+        if self.count <6 {
+            Some(self.count)
+        }
+        else    {
+            None
+        }
+    }
+}
+
