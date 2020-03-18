f401280df2540ccf023c4d73659aeda20abe83d9
Will Wu <willswu@outlook.com>
2020-03-18 20:41:47 +0800

    try consume iter with sum()

--- TryIter/src/lib.rs
+++ TryIter/src/lib.rs
@@ -7,3 +7,11 @@ fn iter_demo(){
     assert_eq!(v1_iter.next().unwrap(),&3);
     assert_eq!(v1_iter.next(),None);
 }
+
+#[test]
+fn iter_sum(){
+    let v1 = vec![1,2,3];
+    let v1_iter = v1.iter();
+    let total:i32 = v1_iter.sum();
+    assert_eq!(total,6);
+}
