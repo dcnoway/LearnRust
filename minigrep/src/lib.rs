4c4685c0ec05e7423cf4cbde6fb89391a3580d2e
Will Wu <willswu@outlook.com>
2020-03-19 02:03:18 +0800

    try some doc with cargo doc --open

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,7 +1,18 @@
+//! # minigrep
+//! minigrep is a tool write by rust beginner's excercise code.
+//!  
+//! It chould search the given file with given query string, 
+//! and print out all the line number and line content which mach the query string.
+//! 
+//! Usage:
+//! ```
+//! minigrep <query string> <file path>
+//! ```
 use std::fs;
 // use std::io;
 use std::error::Error;
 
+/// search func
 pub fn run(cfg: &Config) -> Result<(), Box<dyn Error>> {
     // let mut hit_lines: Vec<&str> = vec![];
     let content = fs::read_to_string(&cfg.filename)?;
@@ -12,6 +23,16 @@ pub fn run(cfg: &Config) -> Result<(), Box<dyn Error>> {
     return Ok(());
 }
 
+/// parsing and holding command line args
+/// # Example
+/// 
+/// ```
+/// use std::env;
+/// let cfg = Config::new(env::args()).unwrap_or_else(|err| {
+///     eprintln!("Arguments error: {:?}", err);
+///     process::exit(1);
+/// });
+/// ```
 pub struct Config {
     pub query: String,
     pub filename: String,
@@ -19,6 +40,11 @@ pub struct Config {
 }
 
 impl Config {
+    /// Construct function, parsing command line args
+    /// # Example
+    /// ```
+    /// let cfg = Config::new(std::env::args()).unwrap();
+    /// ```
     pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
         args.next(); //skip the first arg that is the fullpath of the binary file
         let query: String;
@@ -40,6 +66,7 @@ impl Config {
     }
 }
 
+/// search query string in contents, return a vec holding (line number,line content)
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<(usize, &'a str)> {
     // let mut result: Vec<(usize, &'a str)> = vec![];
     // for (i, line) in contents.lines().enumerate() {
@@ -51,6 +78,7 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<(usize, &'a str)> {
     contents.lines()
         .enumerate()
         .filter(|(_,line)| line.contains(query))
+        .map(|(idx,line)|(idx+1,line))
         .collect()
 }
 
