55fe699472cedd6c8885dda2d9d293a3c639910b
Will Wu <willswu@outlook.com>
2020-03-24 20:33:48 +0800

    temp sync for work on desktop

--- rawmem/src/main.rs
+++ rawmem/src/main.rs
@@ -26,11 +26,24 @@ enum HoldingSliceEnum<'a>{
 
 fn main() {
     println!("Hello, world!");
+    println!("sizeof u32 is:{}",size_of::<u32>());
     println!("sizeof struct pt is:{}",size_of::<Pt>());
-    println!("sizeof struct String is:{}",size_of::<String>());
+
     // println!("sizeof struct str is:{}",size_of::<str>());
-    println!("sizeof struct &str is:{}",size_of::<&str>());
+
     println!("size of enum EmptyEnum is:{}",size_of::<EmptyEnum>());
+
+    println!("size of u8 BYTE is:{}", size_of::<u8>());
     println!("size of enum HoldingOneByteEnum is:{}", size_of::<HoldingOneByteEnum>());
+
+    println!("sizeof struct String is:{}",size_of::<String>());
     println!("size of enum HoldingStringEnum is:{}", size_of::<HoldingStringEnum>());
+
+    println!("sizeof struct &str is:{}",size_of::<&str>());
+    println!("size of enum HoldingSliceEnum is:{}", size_of::<HoldingSliceEnum>());
+
+    let pt =Pt{x:1,y:2};
+    unsafe{
+        //const * rawptr = &pt;
+    }
 }
